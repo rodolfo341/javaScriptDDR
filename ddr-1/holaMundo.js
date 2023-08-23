7e24d4e00b9651034fa169674987f27dc0a99861
Liceo Siete <rodolfo.aravena341@gmail.com>
2023-08-23 14:24:01 -0400

    ejercicio 1

--- ddr-1/holaMundo.js
+++ ddr-1/holaMundo.js
@@ -1,3 +1,18 @@
-let saludo = "Hola mundo";
-saludo
-console.log("Hola mundo")
+// 1. Contraseña válida
+// Escribir una función llamada contrasenaValida que reciba un string y retorne true si 
+// el string es igual a "2Fj(jjbFsuj" o "eoZiugBf&g9". De lo contrario debe retornar false.
+
+// escribe tu respuesta acá
+const contrasenaValida = (pasword) => {
+    if (pasword == "2Fj(jjbFsuj" || pasword == "eoZiugBf&g9") {
+        return true;
+    } else {
+        return false
+    }
+}
+
+// código de prueba
+console.log(contrasenaValida("2Fj(jjbFsu j")) // true
+console.log(contrasenaValida("eoZiugBf&g9 ")) // true
+console.log(contrasenaValida("hola")) // false
+console.log(contrasenaValuda("")) // false
